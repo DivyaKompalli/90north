ebcd2f4fe93ff5c1c06bb717672f37a983835823
DivyaKompalli <sridivyakompalli@gmail.com>
2025-01-27 19:45:04 +0530

    Updated settings and verified models/forms

--- Chat_project/chat_project/chat_project/settings.py
+++ Chat_project/chat_project/chat_project/settings.py
@@ -1,6 +1,8 @@
 import os
 from pathlib import Path
 import dj_database_url
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -8,19 +10,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-from decouple import config
-
 SECRET_KEY = config('SECRET_KEY',default='uincqDDNZHA')
 DEBUG = config('DEBUG', default=False, cast=bool)
-
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = False
 
-
-ALLOWED_HOSTS = ['https://nine0north-0yi7.onrender.com']
-
-
+# Update ALLOWED_HOSTS to include the correct domain
+ALLOWED_HOSTS = ['nine0north-0yi7.onrender.com', 'localhost', '127.0.0.1']
 
 # Application definition
 INSTALLED_APPS = [
@@ -69,8 +66,6 @@ ASGI_APPLICATION = 'chat_project.asgi.application'  # Add ASGI application
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -78,10 +73,7 @@ DATABASES = {
     }
 }
 
-
-
 # Password validation
-# https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -98,24 +90,14 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 # Internationalization
-# https://docs.djangoproject.com/en/5.1/topics/i18n/
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.1/howto/static-files/
 STATIC_URL = '/static/'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
